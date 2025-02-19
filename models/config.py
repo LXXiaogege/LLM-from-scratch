ae3bfd9b5d0cf0862b6f26a31a30ba635be9c8a7
Xin Lv(DNC AI&Technology Platform Dept.-BJ) <lvxin3@asiainfo.com>
2025-02-19 22:05:45 +0800

    moe architecture

--- models/config.py
+++ models/config.py
@@ -16,3 +16,17 @@ class LMConfig:
         self.n_kv_heads = None
         self.flash_attn = False
         self.norm_dim = 768
+
+        self.vocab_size = 50257
+        self.embedding_dim = 768
+        self.n_layers = 12
+
+        # feed forward layer
+        self.multiple_of = 256
+        self.ffn_hidden_dim = None
+        self.ffn_dim = 256
+        self.ffn_dropout = 0.1
+
+        # moe
+        self.n_router_experts = 100
+        self.n_shared_experts = 1
--- models/model.py
+++ models/model.py
@@ -13,35 +13,37 @@ from models.modules import RMSNorm
 import torch.nn.functional as F
 from models.modules import Attention
 from models.config import LMConfig
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 
 class LLMModel(nn.Module):
-    def __init__(self, n_layers):
+    def __init__(self, args: LMConfig):
         super(LLMModel).__init__()
         # embedding layer
-        self.embedding = PositionEmbedding()
+        self.embedding = nn.Embedding(num_embeddings=args.vocab_size, embedding_dim=args.embedding_dim)
         # transformer blocks
         self.transformer_blocks = nn.ModuleList(
-            [TransformerBlock() for _ in range(n_layers)]
+            [TransformerBlock() for _ in range(args.n_layers)]
         )
-        # softmax layer
-        self.softmax = nn.Softmax()
+        self.norm = RMSNorm(args.norm_dim)
+        self.output = nn.Linear(args.embedding_dim, args.vocab_size)
+        # 将模型的多个输出封装成一个对象，以便后续处理。
+        self.OUT = CausalLMOutputWithPast()
 
     def forward(self, inputs):
         embedded = self.embedding(inputs)
         for transformer_block in self.transformer_blocks:
             embedded = transformer_block(embedded)
-        outputs = self.softmax(embedded)
-        return outputs
+        logits = self.output(self.norm(embedded))
 
-
-class PositionEmbedding(nn.Module):
-    def __init__(self, vocab_size, embedding_dim):
-        super(PositionEmbedding).__init__()
-        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
-
-    def forward(self, inputs):
-        return self.embedding(inputs)
+        # 在模型的输出层，通常直接输出 logits，而不是通过 softmax 转换为概率。这是因为：
+        # 1. softmax 通常在训练或推理时的后续步骤中应用，而不是模型内部。
+        # 2. 使用 logits 更加灵活，尤其是对于生成任务（如语言模型），可以根据 logits 做不同的解码操作。
+        # 3. 在训练时，CrossEntropyLoss 会自动处理 softmax，所以模型不需要显式地应用它。
+        self.OUT.__setitem__('logits', logits)
+        # self.OUT.__setitem__('aux_loss', aux_loss)
+        # self.OUT.__setitem__('past_key_values', past_kvs)        __
+        return self.OUT
 
 
 class TransformerBlock(nn.Module):
@@ -67,17 +69,57 @@ class TransformerBlock(nn.Module):
 
 
 class FeedForwardLayer(nn.Module):
-    def __init__(self, dim: int, hidden_dim: int, multiple_of: int, dropout: float):
+    def __init__(self, args: LMConfig):
         super().__init__()
-        if hidden_dim is None:
-            hidden_dim = 4 * dim
-            hidden_dim = int(2 * hidden_dim / 3)
-            hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
-        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
-        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
-        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
-        self.dropout = nn.Dropout(dropout)
+        self.ffn_hidden_dim = args.ffn_hidden_dim
+        if self.ffn_hidden_dim is None:
+            self.ffn_hidden_dim = 4 * args.ffn_dim
+            self.ffn_hidden_dim = int(2 * self.ffn_hidden_dim / 3)
+            self.ffn_hidden_dim = args.multiple_of * ((self.ffn_hidden_dim + args.multiple_of - 1) // args.multiple_of)
+        self.w1 = nn.Linear(args.ffn_dim, self.ffn_hidden_dim, bias=False)
+        self.w2 = nn.Linear(args.ffn_hidden_dim, args.ffn_dim, bias=False)
+        self.w3 = nn.Linear(args.ffn_dim, args.ffn_hidden_dim, bias=False)
+        self.dropout = nn.Dropout(args.ffn_dropout)
 
     def forward(self, x):
         # SiLU 函数的优点在于它能够平滑地处理负值，并且在正值输入时提供较大的输出范围.它有助于保持梯度的流动，避免梯度消失问题。
         return self.dropout(self.w2(F.silu(self.w1(x)) * self.w3(x)))
+
+
+class RouterExpert(nn.Module):
+    """
+    路由专家
+    """
+
+    def __init__(self, args: LMConfig):
+        super(RouterExpert).__init__()
+
+    def forward(self, inputs):
+        pass
+
+
+class RouterModel(nn.Module):
+    """
+    路由层，决定将输入的token送给那个专家
+    通常会选择top K的专家激活
+    """
+
+    def __init__(self, args: LMConfig):
+        super(RouterModel).__init__()
+        self.args = args
+
+    def forward(self, inputs):
+        pass
+
+
+class MOElLayer(nn.Module):
+    def __init__(self, args: LMConfig):
+        super(MOElLayer).__init__()
+        self.args = args
+        #  路由专家
+        self.router_experts = nn.ModuleList([FeedForwardLayer(args) for _ in range(args.n_router_experts)])
+        # 共享专家
+        self.shared_experts = nn.ModuleList([FeedForwardLayer(args) for _ in range(args.n_shared_experts)])
+
+    def forward(self, inputs):
+        pass
